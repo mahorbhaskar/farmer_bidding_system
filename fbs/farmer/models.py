a1cda7ee53073f9f0377095680d65d47e94a6b02
Bhaskar Mahor <bhaskar.mahor.07@gmail.com>
2023-03-20 20:32:05 +0530

    create crop model and applied validation on login page

--- fbs/farmer/models.py
+++ fbs/farmer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Crops(models.Model):
+    """
+        MODEL TO STORE CROP NAMES
+    """
+    crop_name = models.CharField(max_length=100,null=False,blank=False)
+    
+    class Meta:
+        db_table = "crops_name"
---
+++ fbs/static/js/login.js
@@ -0,0 +1,26 @@
+
+$("#login_form").validate({
+    rules: {
+    contact_info: {
+        required: true,
+      },
+      password: {
+        required: true,
+      },
+    },
+    messages: {
+        contact_info: {
+            required: "Please enter your phone number.",
+        },
+        password:{
+            required:"Please enter your password"
+        }
+    },
+    errorClass: "errorClass", //apply a css class for error if you have style for valid
+    validClass: "validClass", //apply a css class for error if you have style for error
+  
+    submitHandler: function (form) {
+    //   form.submit();
+    }
+});
+  
