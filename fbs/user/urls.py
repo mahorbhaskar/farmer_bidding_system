e65c6a32d60b953467dde23b1c17ab8198b064fc
Bhaskar Mahor <bhaskar.mahor.07@gmail.com>
2023-03-20 12:46:41 +0530

    setup login page

--- fbs/user/urls.py
+++ fbs/user/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
 
     path('register/',views.user_register,name='user_register'),
+    path('login/',views.user_login,name='login'),
 
 ]
--- fbs/user/views.py
+++ fbs/user/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 import logging
+from django.contrib.auth.decorators import login_required
+from .models import AuthUserExtension
 
 
 logger = logging.getLogger("error_logger")
@@ -16,3 +18,28 @@ def user_register(request):
     except Exception as e:
         logger.error(f'{repr(e)} -->{request.user.username}')
         return redirect("error404")
+    
+def user_login(request):
+    """
+    Function is to use login for farmer & consumer both
+    """
+    try:
+        if(request.method == "POST"):
+            contact_info = request.POST.get('contact_info')
+            password = request.POST.get('password')
+            print("contact >> ",contact_info)
+            print("password >> ",password)
+            user = AuthUserExtension.objects.filter(mobile=contact_info).exists()
+            print("user >> ",user)
+            if user:
+                print("user exists")
+            else:
+                print("user no exists")
+            print("inside login post")
+            return render(request,'login.html')
+        else:
+            print("inside login get")
+            return render(request,'login.html')
+    except Exception as e:
+        logger.error(f'{repr(e)} -->{request.user.username}')
+        return redirect("error404")        
