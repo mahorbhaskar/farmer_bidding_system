7d74986f45dc7c3aa66ee4bced6f625862be6091
Bhaskar Mahor <bhaskar.mahor.07@gmail.com>
2023-03-20 16:44:09 +0530

    login functionality is complete

--- fbs/user/views.py
+++ fbs/user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 import logging
+from django.contrib import auth,messages
 from django.contrib.auth.decorators import login_required
 from .models import AuthUserExtension
 
@@ -25,21 +26,40 @@ def user_login(request):
     """
     try:
         if(request.method == "POST"):
-            contact_info = request.POST.get('contact_info')
-            password = request.POST.get('password')
-            print("contact >> ",contact_info)
-            print("password >> ",password)
-            user = AuthUserExtension.objects.filter(mobile=contact_info).exists()
-            print("user >> ",user)
-            if user:
-                print("user exists")
+            is_valid = validate_user(request)
+            if is_valid["success"]:
+                return redirect('home')
             else:
-                print("user no exists")
-            print("inside login post")
-            return render(request,'login.html')
+                messages.success(request, is_valid["message"])
+                return render(request,'login.html')
         else:
             print("inside login get")
             return render(request,'login.html')
     except Exception as e:
         logger.error(f'{repr(e)} -->{request.user.username}')
         return redirect("error404")        
+    
+def validate_user(request):
+    """
+        METHOD IS BEING USED TO VALIDATE USER
+    """
+    try:
+        contact_info = request.POST.get('contact_info')
+        password = request.POST.get('password')
+
+        user = AuthUserExtension.objects.filter(mobile=contact_info).exists()
+
+        if user:
+            user = auth.authenticate(username=contact_info,password=password)
+            print("check password >> ",user)
+            if not user:
+                return {"success":False,"message":"Incorrect credentails"}
+            else:
+                auth.login(request,user)
+                return {"success":True,"message":"login successfully"}
+        else:
+            return {"success":False,"message":"Incorrect credentails"}
+
+    except Exception as e:
+        logger.error(f'{repr(e)} -->{request.user.username}')
+        return redirect("error404")
